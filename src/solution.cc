b9a1232af190081bc95344181765db4ce46693b2
rberezhnaia <164949336+rberezhnaia@users.noreply.github.com>
2024-09-15 20:49:49 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -2,8 +2,19 @@
 #include "student.hpp"
 #include <fstream>
 #include <string>
-
+struct Student {
+  std::string full_name;
+  unsigned int uin = 0;
+  double gpa = 0.0;
+};
 Student ReadStudentRecFromStream(std::istream& is) {
-  
-  return Student{};
+  while(is.good()) {
+    unsigned int student_uin = 0;
+    double student_gpa = 0.0;
+    char throw_away = '\0';
+    is >> student_uin >> throw_away >> student_gpa >> throw_away >> student_midterm >> throw_away >> student_final;
+  if (is.fail())
+    break;
+    return Student{};
+  }
 }
