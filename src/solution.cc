e61e51cc6fda1ff001474b88dfb011151eecf669
rberezhnaia <164949336+rberezhnaia@users.noreply.github.com>
2024-09-15 22:46:57 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -10,21 +10,18 @@ struct Student {
 };
 */
 Student ReadStudentRecFromStream(std::istream& is) {
+  std::string line;
+  if (!std::getline(is, line)) {
+    return Student{};
+  }
+  
   std::string first = "";
   std:: string last = "";
   unsigned int uin = 0;
   double gpa = 0.0;
   char comma1 = '\0';
   char comma2 = '\0';
-  is >> first >> last >> comma1 >> uin >> comma2 >> gpa;
-  if (is.fail()) {
-    //break;
-    return Student{};
-  }
-  if (comma1 == ',' && comma2 == ',') {
-    std::string full = first + " " + last;
-    return Student(full, uin, gpa);
-  }
+  
   while(is.good()) {
     //if (is >> first >> last >> comma1 >> uin >> comma2 >> gpa) {
     is >> first >> last >> comma1 >> uin >> comma2 >> gpa;
