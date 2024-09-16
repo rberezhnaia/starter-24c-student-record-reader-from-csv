04d418efcf34d91303db53ffbe2bdaf3db6d1b86
rberezhnaia <164949336+rberezhnaia@users.noreply.github.com>
2024-09-15 21:35:00 -0500

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -16,16 +16,17 @@ Student ReadStudentRecFromStream(std::istream& is) {
   double gpa = 0.0;
   char comma1 = '\0';
   char comma2 = '\0';
-  //while(is.good()) {
-  if (is >> first >> last >> comma1 >> uin >> comma2 >> gpa) {
-    if (is.fail()) {
-      //break;
-      return Student{};
-    }
-    if (comma1 == ',' && comma2 == ',') {
-      std::string full = first + " " + last;
-      return Student(full, uin, gpa);
-    }
-  } 
+  while(is.good()) {
+    if (is >> first >> last >> comma1 >> uin >> comma2 >> gpa) {
+      if (is.fail()) {
+        //break;
+        return Student{};
+      }
+      if (comma1 == ',' && comma2 == ',') {
+        std::string full = first + " " + last;
+        return Student(full, uin, gpa);
+      }
+    } 
+  }
   return Student{};
 }
